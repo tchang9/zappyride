bd4756ac91703a07781c3ce8a1616e2428551a41
tchang9 <tlchang9@gmail.com>
2019-11-17 22:52:24 -0600

    fixed if the api returns no stations

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ class App extends Component{
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {this.renderStations()}
+          {this.state.stations ? this.renderStations() : null}
         </Map>
         <Card style={{position: "absolute"}} body className="message-form">
           <CardTitle>Electric Stations in Washington</CardTitle>
